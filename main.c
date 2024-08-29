d8d9384faeb6b7f2ff143622dca12a6cabfa4b9c
Kana <109441902+ai-kana@users.noreply.github.com>
2024-08-29 16:54:16 -0700

    Add cleanup

--- main.c
+++ main.c
@@ -12,9 +12,14 @@ struct Task {
 };
 
 static _Atomic(int) workptr = -1;
+static _Atomic(bool) exiting = false;
 static struct Task* work[10];
 static void* thread_worker(void* restrict arg) {
     while (true) {
+        if (exiting) {
+            break;
+        }
+
         if (workptr == -1) {
             continue;
         }
@@ -22,6 +27,7 @@ static void* thread_worker(void* restrict arg) {
         struct Task* task = work[workptr];
         task->result = task->function();
         task->isCompleted = true;
+        workptr--;
     }
 
     return NULL;
@@ -54,14 +60,6 @@ static void run_async(register struct Task* restrict task) {
     while (!task->isCompleted);
 }
 
-static void run_sync(register struct Task* restrict task) {
-    if (task->isCompleted) {
-        return;
-    }
-
-    task->result = task->function();
-}
-
 static void* test() {
     int* x = malloc(sizeof(int));
     *x = 7;
@@ -80,4 +78,8 @@ int main() {
     int *p = task.result;
     printf("Result: %d\n", *p);
     free(p);
+
+    exiting = true;
+    void* ret;
+    pthread_join(pool_data.thread, &ret);
 }
